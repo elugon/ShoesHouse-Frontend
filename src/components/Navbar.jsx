acfafa528f2cbd083bb2a8332d366140cb27ee5a
Elu Gonzalez <elugon10@gmail.com>
2022-09-14 23:56:38 +0200

    home page css in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,24 @@
 import React, { useContext } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
+import shoesHouseLogo from '../img/shoesHouseLogo.png'
 
 export default function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <div>
+    <div className='flex items-center px-2 border-solid border-2 rounded h-12 '>
       {user && <p>Hello {user.username}</p> }
-      <ul>
-        <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/">Home</NavLink></li>
-        <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/shoes">Shoes</NavLink></li>
-        {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/signup">Sign up</NavLink></li>}
-        {!isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/login">Login</NavLink></li>}{
-        /* {isLoggedIn && <li><NavLink className={(element) => element.isActive ? 'selected' : ''} to="/mycar">My Car</NavLink></li>} */}
-        {isLoggedIn && <li><button onClick={() => logOutUser()}>Log out</button></li>}
-        <li><button onClick={() => navigate(-1)}>Go back</button></li>
-      </ul>
+      <nav className='flex w-full h-full items-center place-content-between font-serif'>
+        <img src={shoesHouseLogo} alt='logo of ecommerce' className='object-contain w-12 h-12'></img>
+        <NavLink className={(element) => element.isActive ? 'selected' : ''} to="/">Home</NavLink>
+        <NavLink className={(element) => element.isActive ? 'selected' : ''} to="/shoes">Shoes</NavLink>
+        {!isLoggedIn && <NavLink className={(element) => element.isActive ? 'selected' : ''} to="/signup">Sign up</NavLink>}
+        {!isLoggedIn && <NavLink className={(element) => element.isActive ? 'selected' : ''} to="/login">Login</NavLink>}{
+        /* {isLoggedIn && <NavLink className={(element) => element.isActive ? 'selected' : ''} to="/mycar">My Car</NavLink>} */}
+        {isLoggedIn && <button onClick={() => logOutUser()}>Log out</button>}
+        <button onClick={() => navigate(-1)}>Go back</button>
+      </nav>
     </div>
   )
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import imageBackground from '../../src/img/shoes-background.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div className='h-screen w-screen bg-contain bg-repeat bg-left-top object-scale-down' style={{backgroundImage: `url(${imageBackground})`}}></div>
+    </>
+    
   )
 }
